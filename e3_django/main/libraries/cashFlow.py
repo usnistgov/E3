471364a018f647e436017f022b33cc438848e6d1
Tasha Kim <tasha_kim@brown.edu>
2021-02-19 15:16:47 -0500

    Slight addition

---
+++ e3_django/main/libraries/cashFlow.py
@@ -0,0 +1,35 @@
+from ../models/bcn as bcn
+
+def blankFlow(studyPeriod, timestepValue):
+    timestepCount = studyPeriod / timestepValue
+    return #zeroes(timestepCount + 1)
+
+
+def bcnFlow(bcnObject, studyPeriod, timestepCount):
+    if bcnObject.recurBool = False:
+        bcnFlowNonRecur(bcnObject, discountRate)
+    else:
+        bcnFlowNonRecur(bcnObject, discountRate)
+
+def bcnFlowNonRecur(bcnObject, discountRate):
+    bcnFlowNonDisc = blankFlow(studyPeriod, timestepValue)
+    bcnFlowDisc = blankFlow(studyPeriod, timestepValue)
+    quantList = blankFlow(studyPeriod, timestepValue)
+
+    if not bcnObject.valuePerQ:
+        return bcnFlowNonDisc, bcnFlowDisc
+    if bcnObject.quantVarValue:
+        # quantEsc = 
+    def escalatedQuantCalc(quantity, quantVarRateType, quantVarValue, initialOcc):
+
+        reurn bcnFlowNonDisc, bcnFlowDisc, quantList
+
+    return
+
+def bcnFlowRecur(bcnObject, discountRate, timestep):
+    bcnFlowNonDisc = blankFlow(studyPeriod, timestepValue)
+    bcnFlowDisc = blankFlow(studyPeriod, timestepValue)
+    recurList = blankFlow(studyPeriod, timestepValue)
+    quantList = blankFlow(studyPeriod, timestepValue)
+
+    
