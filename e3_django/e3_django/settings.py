7588bad5a68baf61236c8d36a47ce9a80bd7599d
Luke Donmoyer <lukedonmoyer@gmail.com>
2021-06-08 09:23:10 -0400

    Added new requirements

--- e3_django/e3_django/settings.py
+++ e3_django/e3_django/settings.py
@@ -42,8 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     # created
-    'API',
+    'API.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
