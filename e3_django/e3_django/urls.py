99a53e1a93e0635084086d8c063d6186c266b02d
Luke Donmoyer <lukedonmoyer@gmail.com>
2021-07-13 14:43:07 -0400

    Added favicon

--- e3_django/e3_django/urls.py
+++ e3_django/e3_django/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('API.urls')),
     path('admin/', admin.site.urls),
-    path('', include('frontend.urls'))
+    path('', include('frontend.urls')),
 ]
