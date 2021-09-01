2ca779bd24d8a5b5b9d52273ff41af131426f388
Tasha Kim <tasha_kim@brown.edu>
2021-08-31 21:01:14 -0400

    Update

--- e3_django/API/tests/test_alternativeSummary.py
+++ e3_django/API/tests/test_alternativeSummary.py
@@ -1,10 +1,64 @@
-from django.test import TestCase
-from API.models import AlternativeSummary
+#from django.test import TestCase
+#from API.models import AlternativeSummary
+from decimal import Decimal
+from unittest import TestCase
+
+from API.objects import AlternativeSummary
+
+PLACES = Decimal(10) ** -4
 
 """
 AlternativeSummary tests here using django test.
 """
+class NewAlternativeSummaryTest(TestCase):
+    def setUp(self):
+        self.alt_summary0 = AlternativeSummary(
+            altID = 1,
+            totalBenefits = 139.69,
+            totalCosts = 1580.29,
+            totalCostsInv = 431.30,
+            totalCostsNonInv = 1148.99,
+            netBenefits = -301.34,
+            netSavings = -336.34,
+            SIR = "Not Calculable",
+            IRR = None,
+            AIRR = "Not Calculable",
+            SPP = "Infinity",
+            DPP = "Infinity",
+            BCR = "Not Calculable",
+            quantSum = [807.987767, 1100],
+            quantUnits = [["Tag 1", ",^3"], ["Tag 3", "m"]],
+            MARR = 0.04,
+            deltaQuant = [["Tag 1",707.9877672], ["Tag 3",1100]],
+            nsDeltaQuant = [["Tag 1",-0.4750678],["Tag 3",-0.3057656]],
+            nsPercQuant = [["Tag 1",-41.627135],["Tag 3","Infinity"]],
+            nsElasticityQuant = [["Tag 1",-0.0334636],["Tag 3","Infinity"]]
+        )
 
+        self.alt_summary1 = AlternativeSummary(
+            altID = 5,
+            totalBenefits = -123.64,
+            totalCosts = 234.64,
+            totalCostsInv = 890.34,
+            totalCostsNonInv = 234.23,
+            netBenefits = 432.56, 
+            netSavings = 234.64,
+            SIR = 1.42,
+            IRR = 0.043,
+            AIRR = 0.234,
+            SPP = 3,
+            DPP = 4,
+            BCR = 1.42,
+            quantSum = [-98.02, 231.2],
+            quantUnits = [["Tag 1", ",^3"], ["Tag 3", "m"]],
+            MARR = 0.04,
+            deltaQuant = [["Tag 1",707.9877672], ["Tag 3",1100]],
+            nsDeltaQuant = [["Tag 1",0.2334], ["Tag 3",1.2342]],
+            nsPercQuant = [["Tag 1",45.342], ["Tag 3",-0.3242]],
+            nsElasticityQuant = [["Tag 1",23.534], ["Tag 3",34.2343]]
+        )
+
+"""
 class AlternativeSummary(TestCase):
     def create_model(self):
         """
@@ -73,3 +127,4 @@ class AlternativeSummary(TestCase):
 
         print("\nNew AlternativeSummary object was created.")
         print(">>> Passed AlternativeSummary tests!")
+"""
