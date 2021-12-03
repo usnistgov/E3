0c444dce61211e6491fc5ce3b59875e8e7083b75
Luke Donmoyer <lukedonmoyer@gmail.com>
2021-12-03 08:45:12 -0500

    Pulled SECRET_KEY out into .env file

--- e3_django/e3_django/settings.py
+++ e3_django/e3_django/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yzp=q+*3e--r4ie+hzgj_@2$auyfk38l4#@1jj6tgshv0)dga2jn'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
