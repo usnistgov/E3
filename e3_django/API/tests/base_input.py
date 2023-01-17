7614eb39290045edd7befe796635d8a261ab54fb
David Webb <david.webb@nist.gov>
2023-01-17 09:51:02 -0500

    Clean up

--- e3_django/API/tests/base_input.py
+++
@@ -1,6 +0,0 @@
-class BaseInput:
-    def __init__(self,sensitivityObjects,analysisObject,bcnObjects,alternativeObjects):
-        self.sensitivityObjects = sensitivityObjects
-        self.analysisObject = analysisObject
-        self.bcnObjects = bcnObjects
-        self.alternativeObjects = alternativeObjects
